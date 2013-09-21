4bb4d698a95478fd87a5a479315b796671449e81
gps13 <g.psistakis@gmail.com>
2013-09-21 23:51:50 +0300

    asses 1 init commit

---
+++ assesments/asses_1/asses_1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace asses_1
+{
+    /// <summary>
+    /// This is a programming assignment for "Beginning Game Programming with C#"
+    /// The program does the following: 
+    /// Imagine you’re developing a game that keeps statistics about player performance. 
+    /// Everyone wants to be able to see how much gold they've earned.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //welcome message
+            Console.WriteLine("Welcome! This app calculates your average gold-collecting performance");
+            // Prompt 
+            Console.Write("Please provide total gold you've collected in the game: ");
+            int gold = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine("Points/sec: " + gold);
+            
+
+        }
+    }
+}
---
+++ assessm_1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace first_sharp
+{
+    /// <summary>
+    /// class that prints
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// main method should print
+        /// </summary>
+        /// <param name="args">command line args</param>
+        static void Main(string[] args)
+        {
+            //declare vars
+            int score = 1356;
+            int totalsecondsPlayed = 10000;
+
+            //calc ponts/sec
+            float pointspersecond = (float)score / totalsecondsPlayed;
+            Console.WriteLine("Points/sec: " + pointspersecond);
+            Console.WriteLine();
+        }
+    }
+}
