1de9c025fc9d26f818a1fcad7a0a8d1d9575dc70
gps13 <g.psistakis@gmail.com>
2013-09-29 23:54:11 +0300

    added lab 4

---
+++ Labs/gameprogramming-labs-Lab4Code/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // create a new deck and print the contents of the deck
+            Console.WriteLine("----- A New deck -----");
+            Deck deck = new Deck();
+            deck.Print();
+            Console.WriteLine();
+            // shuffle the deck and print the contents of the deck
+            Console.WriteLine("----- Shuffle & print -----");
+            deck.Shuffle();
+            deck.Print();
+            Console.WriteLine();
+            // take the top card from the deck and print the card rank and suit
+            Console.WriteLine("----- take the top card & print -----");
+            Card card = deck.TakeTopCard();
+            Console.WriteLine(card.Rank + " of " + card.Suit);
+            Console.WriteLine();
+
+            // take the top card from the deck and print the card rank and suit
+            Console.WriteLine("----- take another top card & print -----");
+            card = deck.TakeTopCard();
+            Console.WriteLine(card.Rank + " of " + card.Suit);
+            Console.WriteLine();
+
+        }
+    }
+}
