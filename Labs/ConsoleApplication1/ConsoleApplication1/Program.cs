af9e78d54652790cde896d9bf027a732d00c5ce8
gps13 <g.psistakis@gmail.com>
2013-09-30 23:23:11 +0300

    labs 6 added

---
+++ Labs/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //prompt and read gamertag
+            Console.Write("Enter gamertag: ");
+            string gamertag = Console.ReadLine();
+
+            //prompt and read level
+            Console.Write("ENTER LEVEL: ");
+            int level = int.Parse(Console.ReadLine());
+
+
+            //extract first char of gamertag
+            char firstGamertagChar = gamertag[0];
+
+
+
+            //print valuues
+            Console.WriteLine();
+            Console.WriteLine("gamertag: " + gamertag);
+            Console.WriteLine("Level: " + level);
+            Console.WriteLine("first gamertag char: " + firstGamertagChar);
+            Console.WriteLine();
+        }
+    }
+}
