26bc231fb8b6cec69dccee6246e414940498cac0
gps13 <g.psistakis@gmail.com>
2013-10-02 23:12:16 +0300

    Lab 7

---
+++ Labs/lab7/lab7/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace lab7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("month is your birthday? ");
+            string answerMonth = Console.ReadLine();
+            Console.Write("day is your birthday? ");
+            int answerDay = int.Parse(Console.ReadLine());
+            Console.Write("You’ll receive an email reminder on " + answerMonth + " " + (answerDay-1));
+            Console.WriteLine();
+        }
+    }
+}
